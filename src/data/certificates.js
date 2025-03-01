76bd2600564d9c279ea33bdee447be113fe05101
KhushiChauhan8 <kc6478576@gmail.com>
2025-03-01 22:27:58 +0530

    enhence contact section

--- src/data/certificates.js
+++ src/data/certificates.js
@@ -3,8 +3,6 @@ export const certificatesData = [
   { id: 2, Img: "/certificate2.jpg" },
   { id: 3, Img: "/certificate3.jpg", Description: "This is the description for Certificate 3." },
   { id: 4, Img: "/certificate4.jpg" },
-  { id: 5, Img: "/certificate5.jpg" },
-  { id: 6, Img: "/certificate6.jpg", Title: "Certificate 6" },
-  { id: 7, Img: "/certificate7.jpg" },
-  { id: 8, Img: "/certificate8.png" },
+  { id: 7, Img: "/certificate5.jpg" },
+  { id: 8, Img: "/certificate6.png" },
 ];
